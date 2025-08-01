be818fc2c97844c6e9e073294edda74e00525631
Till Biskup <till@till-biskup.de>
2025-08-01 17:01:33 +0200

    Add relevant parts of file entities module from evedata

--- evefile/entities/file.py
+++ evefile/entities/file.py
@@ -1,3 +1,240 @@
 """
-file module of the evefile package.
+
+*Entities representing an eveH5 file on the entire file level.*
+
+While the entities in this module represent the contents of an eveH5 file,
+they clearly abstract from the internal structure of these files.
+Furthermore, there are different versions of the underlying schema
+(*i.e.*, organisation) of these files, and the entities abstract away from
+these differences as well. The key concept is to provide users of the
+``evedata`` interface with useful abstractions allowing to conveniently
+access all the data present in an eveH5 file.
+
+
+Overview
+========
+
+A first overview of the classes implemented in this module and their
+hierarchy is given in the UML diagram below.
+
+
+.. figure:: /uml/evefile.entities.file.*
+    :align: center
+
+    Class hierarchy of the :mod:`evefile.entities.file` module. The
+    :class:`File` class is sort of the central interface to the entire
+    subpackage, as this class provides a faithful representation of all
+    information available from a given eveH5 file. To this end,
+    it incorporates instances of classes of the other modules of the
+    subpackage. Furthermore, "Scan" inherits from the identically named
+    facade of the scan functional layer and contains the full information
+    of the SCML file (if the SCML file is present in the eveH5 file).
+
+
+Module documentation
+====================
+
 """
+
+import datetime
+import logging
+
+import evedata.scan.boundaries.scan
+
+logger = logging.getLogger(__name__)
+
+
+class File:
+    """
+    Representation of all information available from a given eveH5 file.
+
+    Individual measurements are saved in HDF5 files using a particular
+    schema (eveH5). Besides file-level metadata, there are log messages,
+    a scan description (originally an XML/SCML file), and the actual data.
+
+    The data are organised in three functionally different sections: data,
+    snapshots, and monitors.
+
+
+    Attributes
+    ----------
+    metadata : :class:`Metadata`
+        File metadata
+
+    log_messages : :class:`list`
+        Log messages from an individual measurement
+
+        Each item in the list is an instance of :class:`LogMessage`.
+
+    snapshots : :class:`dict`
+        Device data recorded as snapshot during a measurement.
+
+        Each item is an instance of
+        :class:`evefile.entities.data.Data`.
+
+    monitors : :class:`dict`
+        Device data monitored during a measurement.
+
+        Each item is an instance of
+        :class:`evefile.entities.data.Data`.
+
+    position_timestamps : :class:`evefile.entities.data.TimestampData`
+        Timestamps for each individual position.
+
+        Monitors have timestamps (milliseconds since start of the scan)
+        rather than positions as primary quantisation axis. This object
+        provides a mapping between timestamps and positions and can be used
+        to map monitor data to positions.
+
+
+    Examples
+    --------
+    The :class:`File` class is not meant to be used directly, as any
+    entities, but rather indirectly by means of the respective facades in
+    the boundaries technical layer of the :mod:`evefile` subpackage.
+    Hence, for the time being, there are no dedicated examples how to use
+    this class. Of course, you can instantiate an object as usual.
+
+    """
+
+    def __init__(self):
+        self.metadata = Metadata()
+        self.log_messages = []
+        self.data = {}
+        self.snapshots = {}
+        self.monitors = {}
+        self.position_timestamps = None
+
+
+class Metadata:
+    """
+    Metadata of a given eveH5 file.
+
+    As measurements result in individual files, there is a series of
+    crucial metadata of such a measurement on this global level.
+
+
+    Attributes
+    ----------
+    filename : :class:`str`
+        Name (full path) of the eveH5 file.
+
+    eveh5_version : :class:`str`
+        Version of the eveH5 schema.
+
+    eve_version : :class:`str`
+        Version of the eve engine used to record the data.
+
+    xml_version : :class:`str`
+        Version of the schema used for the scan description (SCML/XML)
+
+    measurement_station : :class:`str`
+        Name of the measurement station used to record the data.
+
+    start : :class:`datetime.datetime`
+        Timestamp of the start of the measurement
+
+    end : :class:`datetime.datetime`
+        Timestamp of the end of the measurement
+
+    description : :class:`str`
+        User-entered description of the entire scan.
+
+    simulation : :class:`bool`
+        Flag signalling whether the measurement was a simulation.
+
+        Default: ``False``
+
+    preferred_axis : :class:`string`
+        Name of the axis marked as preferred in the scan description.
+
+        Default: ""
+
+    preferred_channel : :class:`string`
+        Name of the channel marked as preferred in the scan description.
+
+        Default: ""
+
+    preferred_normalisation_channel : :class:`string`
+        Name of the channel marked as preferred for normalising.
+
+        Default: ""
+
+    Examples
+    --------
+    The :class:`Metadata` class is not meant to be used directly, as any
+    entities, but rather indirectly by means of the respective facades in
+    the boundaries technical layer of the :mod:`evefile` subpackage.
+    Hence, for the time being, there are no dedicated examples how to use
+    this class. Of course, you can instantiate an object as usual.
+
+    """
+
+    def __init__(self):
+        self.filename = ""
+        self.eveh5_version = ""
+        self.eve_version = ""
+        self.xml_version = ""
+        self.measurement_station = ""
+        self.start = datetime.datetime.now()
+        self.end = datetime.datetime.now()
+        self.description = ""
+        self.simulation = False
+        self.preferred_axis = ""
+        self.preferred_channel = ""
+        self.preferred_normalisation_channel = ""
+
+
+class LogMessage:
+    """
+    Log message from an individual measurement.
+
+    Operators can enter log messages during a measurement using the
+    eve-gui. In such case, the respective message appears in the eveH5
+    file together with a timestamp.
+
+
+    Attributes
+    ----------
+    timestamp : :class:`datetime.datetime`
+        Timestamp of the log message
+
+    message : :class:`str`
+        Actual content of the log message.
+
+
+    Examples
+    --------
+    The :class:`Scan` class is not meant to be used directly, as any
+    entities, but rather indirectly by means of the respective facades in
+    the boundaries technical layer of the :mod:`evefile` subpackage.
+    Hence, for the time being, there are no dedicated examples how to use
+    this class. Of course, you can instantiate an object as usual.
+
+    """
+
+    def __init__(self):
+        self.timestamp = datetime.datetime.now()
+        self.message = ""
+
+    def from_string(self, string=""):
+        """
+        Set attributes from string.
+
+        In eveH5 files up to v7, the log messages are single strings with
+        the ISO timestamp at the beginning, followed by the actual message.
+        Timestamp and message are separated by ": ".
+
+        This method separates both parts and converts the timestamp into an
+        actual :obj:`datetime.datetime` object, consistent with the
+        :attr:`timestamp` attribute.
+
+        Parameters
+        ----------
+        string : :class:`str`
+            Log message consisting of timestamp and actual message.
+
+        """
+        timestamp, message = string.split(": ", maxsplit=1)
+        self.timestamp = datetime.datetime.fromisoformat(timestamp)
+        self.message = message
--- tests/entities/test_file.py
+++ tests/entities/test_file.py
@@ -1,3 +1,75 @@
+import datetime
 import unittest
 
 from evefile.entities import file
+
+
+class TestFile(unittest.TestCase):
+    def setUp(self):
+        self.file = file.File()
+
+    def test_instantiate_class(self):
+        pass
+
+    def test_has_attributes(self):
+        attributes = [
+            "metadata",
+            "log_messages",
+            "snapshots",
+            "monitors",
+            "position_timestamps",
+        ]
+        for attribute in attributes:
+            with self.subTest(attribute=attribute):
+                self.assertTrue(hasattr(self.file, attribute))
+
+
+class TestMetadata(unittest.TestCase):
+    def setUp(self):
+        self.metadata = file.Metadata()
+
+    def test_instantiate_class(self):
+        pass
+
+    def test_has_attributes(self):
+        attributes = [
+            "filename",
+            "eveh5_version",
+            "eve_version",
+            "xml_version",
+            "measurement_station",
+            "start",
+            "end",
+            "description",
+            "simulation",
+            "preferred_axis",
+            "preferred_channel",
+            "preferred_normalisation_channel",
+        ]
+        for attribute in attributes:
+            with self.subTest(attribute=attribute):
+                self.assertTrue(hasattr(self.metadata, attribute))
+
+
+class TestLogMessage(unittest.TestCase):
+    def setUp(self):
+        self.log_message = file.LogMessage()
+
+    def test_instantiate_class(self):
+        pass
+
+    def test_has_attributes(self):
+        attributes = ["timestamp", "message"]
+        for attribute in attributes:
+            with self.subTest(attribute=attribute):
+                self.assertTrue(hasattr(self.log_message, attribute))
+
+    def test_from_string_sets_timestamp_and_message(self):
+        string = "2024-07-25T10:04:03: Lorem ipsum"
+        self.log_message.from_string(string)
+        timestamp, message = string.split(": ", maxsplit=1)
+        self.assertEqual(
+            datetime.datetime.fromisoformat(timestamp),
+            self.log_message.timestamp,
+        )
+        self.assertEqual(message, self.log_message.message)
