f5ce8e65fc0f44a3630e79ae027307e890c3a825
Till Biskup <till@till-biskup.de>
2025-08-02 15:07:14 +0200

    Add module documentation, but not yet any code

--- evefile/boundaries/evefile.py
+++ evefile/boundaries/evefile.py
@@ -1,3 +1,119 @@
 """
-evefile module of the evefile package.
+
+*High-level Python object representation of eveH5 file contents.*
+
+.. sidebar:: Contents
+
+    .. contents::
+        :local:
+        :depth: 1
+
+This module provides a high-level representation of the contents of an eveH5
+file. Being a high-level, user-facing object representation, technically
+speaking this module is a facade. The corresponding resource
+(persistence-layer-facing interface) would be the :mod:`eveh5
+<evefile.boundaries.eveh5>` module.
+
+
+Overview
+========
+
+A first overview of the classes implemented in this module and their
+hierarchy is given in the UML diagram below.
+
+
+.. figure:: /uml/evefile.boundaries.evefile.*
+    :align: center
+
+    Class hierarchy of the :mod:`evefile.boundaries.evefile` module,
+    providing the facade (user-facing interface) for an eveH5 file.
+    Basically, it inherits from :class:`evefile.entities.file.File`
+    and adds behaviour. Most of this behaviour is contributed by the various
+    modules of the :mod:`controllers <evefile.controllers>`
+    subpackage.
+
+
+Key aspects
+===========
+
+While the :mod:`evefile <evefile.boundaries.evefile>` module is the
+high-level interface (facade) of the ``evefile`` package,
+it is still, from a functional viewpoint, close to the actual eveH5 files,
+providing a faithful representation of all information contained in an eveH5
+(and SCML) file. Nevertheless, it is clearly an abstraction from the actual
+data files. Hence, the key characteristics of the module are:
+
+* Stable interface to eveH5 files, regardless of their version.
+
+  * Some features may only be available for newer eveH5 versions, though.
+
+* Powerful abstractions on the device level.
+
+  * Options to devices appear as attributes of the device objects, not as
+    separate datasets.
+
+* Actual **data are loaded on demand**, not when loading the file.
+
+  * This does *not* apply to the metadata of the individual datasets.
+    Those are read upon reading the file.
+  * Reading data on demand should save time and resources, particularly
+    for larger files.
+  * Often, you are only interested in a subset of the available data.
+
+
+Usage
+=====
+
+Loading the contents of a data file of a measurement may be as simple as:
+
+.. code-block::
+
+    from evefile.boundaries.evefile import EveFile
+
+    evefile = EveFile()
+    evefile.load(filename="my_measurement_file.h5")
+
+Of course, you could alternatively set the filename first,
+thus shortening the :meth:`load` method call:
+
+.. code-block::
+
+    evefile = EveFile()
+    evefile.filename = "my_measurement_file.h5"
+    evefile.load()
+
+There is even a third way now: Instantiating the class already with a
+given filename:
+
+.. code-block::
+
+    evefile = EveFile(filename="my_measurement_file.h5")
+    evefile.load()
+
+And yes, you can of course chain the object creation and loading the file
+if you like. However, this leads to harder to read code and is therefore
+*not* suggested.
+
+
+Internals: What happens when reading an eveH5 file?
+===================================================
+
+Reading an eveH5 file is not as simple as reading contents of an HDF5 file
+and present its contents as Python object hierarchy. At least, if you would
+like to view, process, and analyse your data more conveniently, you should
+not stop here. The idea behind the ``evedata`` package, and in parts behind
+the :class:`EveFile` class, is to provide you as consumer of the data with
+powerful abstractions and structured information. To this end, a series of
+steps are necessary:
+
+* Read the eveH5 file (actually, an HDF5 file).
+* Get the correct :class:`VersionMapper
+  <evefile.controllers.version_mapping.VersionMapper>` class.
+* Map the file contents to the proper data structures provided by the
+  ``evedata`` package.
+
+
+Module documentation
+====================
+
 """
--- evefile/controllers/joining.py
+++ evefile/controllers/joining.py
@@ -1,3 +1,151 @@
 """
-joining module of the evefile package.
+
+*Ensure data and axes values are commensurate and compatible.*
+
+.. sidebar:: Contents
+
+    .. contents::
+        :local:
+        :depth: 2
+
+For each motor axis and detector channel, in the original eveH5 file only
+those values appear---together with a "position" (PosCount) value---that
+have actually been set or measured. Hence, the number of values (*i.e.*,
+the length of the data vector) will generally be different for different
+devices. To be able to plot arbitrary data against each other,
+the corresponding data vectors need to be commensurate. If this is not the
+case, they need to be brought to the same dimensions (*i.e.*, "joined",
+originally somewhat misleadingly termed "filled").
+
+To be exact, being commensurate is only a necessary, but not a sufficient
+criterion, as not only the shape needs to be commensurate, but the indices
+(in this case the positions) be identical.
+
+
+A bit of history
+================
+
+In the previous interface (``EveFile``), there are four "fill modes" available
+for data: NoFill, LastFill, NaNFill, LastNaNFill. From the `documentation of
+eveFile <https://www.ahf.ptb.de/messpl/sw/python/common/eveFile/doc/html
+/Section-Fillmode.html#evefile.Fillmode>`_:
+
+
+NoFill
+    "Use only data from positions where at least one axis and one channel
+    have values."
+
+    Actually, not a filling, but mathematically an intersection, or,
+    in terms of relational databases, an ``SQL INNER JOIN``. In any case,
+    data are *reduced*.
+
+LastFill
+    "Use all channel data and fill in the last known position for all axes
+    without values."
+
+    Similar to an ``SQL LEFT JOIN`` with data left and axes right,
+    but additionally explicitly setting the missing axes values in the join
+    to the last known axis value.
+
+NaNFill
+    "Use all axis data and fill in NaN for all channels without values."
+
+    Similar to an ``SQL LEFT JOIN`` with axes left and data right. To be
+    exact, the ``NULL`` values of the join operation will be replaced by
+    ``NaN``.
+
+LastNaNFill
+    "Use all data and fill in NaN for all channels without values and fill
+    in the last known position for all axes without values."
+
+    Similar to an ``SQL OUTER JOIN``, but additionally explicitly setting
+    the missing axes values in the join to the last known axis value and
+    replacing the ``NULL`` values of the join operation by ``NaN``.
+
+
+Furthermore, for the Last*Fill modes, snapshots are inspected for axes
+values that are newer than the last recorded axis in the main/standard section.
+
+Note that none of the fill modes guarantees that there are no NaNs (or
+comparable null values) in the resulting data.
+
+
+.. important::
+
+    The IDL Cruncher seems to use LastNaNFill combined with applying some
+    "dirty" fixes to account for scans using MPSKIP and those scans
+    "monitoring" a motor position via a pseudo-detector. The ``EveHDF``
+    class (DS) uses LastNaNFill as a default as well but does *not* apply
+    some additional post-processing.
+
+    Shall fill modes be something to change in a viewer? And which fill
+    modes are used in practice (and do we have any chance to find this out)?
+
+
+How to deal with missing values?
+================================
+
+Depending on the concrete situation, there may be no value available to
+fill a gap in an axis. Hence, how to deal with this situation?
+
+
+Numeric values
+--------------
+
+For numeric values, some kind of "NaN" (not a number) could be used.
+
+For NumPy, only floats can have a dedicated "NaN", but no other dtype.
+Hence, in case of missing values, a masked array (
+:class:`numpy.ma.MaskedArray`) is used and :data:`numpy.ma.masked` set
+explicitly for those missing values. For all practical purposes,
+this should work similar to the :data:`numpy.nan`. In particular,
+when trying to plot a :class:`numpy.ma.MaskedArray`, the masked values are
+simply ignored. For further details of how to work with masked arrays,
+see the :mod:`numpy.ma` documentation.
+
+
+Non-numeric values
+------------------
+
+First of all: Does this situation occur in reality? Yes, there are axes
+with non-numeric values. But are these axes ever joined? If so,
+some textual value such as "N/A" (not available) may be used.
+
+.. note::
+
+    The default fill value of a :class:`numpy.ma.MaskedArray` is ``N/A``,
+    and this is (only) used when calling :meth:`numpy.ma.MaskedArray.filled`.
+    Otherwise, the masked values are in most cases simply ignored. For an
+    overview of the default fill values of masked arrays, see the
+    :attr:`numpy.ma.MaskedArray.fill_value` attribute.
+
+
+Join modes currently implemented
+================================
+
+Currently, there is exactly no join mode implemented:
+
+
+For developers
+==============
+
+To implement additional join modes, create a class inheriting from the
+:class:`Join` base class and implement the actual joining in the private
+method ``_join()``.
+
+There is a factory class :class:`JoinFactory` that you can ask to get a
+:class:`Join` object:
+
+.. code-block::
+
+    factory = JoinFactory()
+    join = factory.get_join(mode="AxesLastFill")
+
+This would return an :obj:`AxesLastFill` object. For further details,
+see the :class:`JoinFactory` documentation.
+
+
+Module documentation
+====================
+
 """
