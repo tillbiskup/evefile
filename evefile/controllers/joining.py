81a7ded3100ddfed82b880c6891287034210f1cd
Till Biskup <till@till-biskup.de>
2025-08-07 12:55:48 +0200

    Populate join module

--- evefile/controllers/joining.py
+++ evefile/controllers/joining.py
@@ -149,3 +149,332 @@ Module documentation
 ====================
 
 """
+
+import numpy as np
+from numpy import ma
+
+
+class Join:
+    """
+    Base class for joining data.
+
+    For each motor axis and detector channel, in the original eveH5 file only
+    those values appear---together with a "position counter" (PosCount)
+    value---that have actually been set or measured. Hence, the number of
+    values (*i.e.*, the length of the data vector) will generally be different
+    for different devices. To be able to plot arbitrary data against each
+    other, the corresponding data vectors need to be commensurate. If this
+    is not the case, they need to be brought to the same dimensions (*i.e.*,
+    "joined", originally somewhat misleadingly termed "filled").
+
+    The main "quantisation" axis of the values for a device and the
+    common reference is the list of positions. Hence, to join,
+    first of all the lists of positions are compared, and gaps handled
+    accordingly.
+
+    As there are different strategies how to deal with gaps in the
+    positions list, generally, there will be different subclasses of the
+    :class:`Join` class dealing each with a particular strategy.
+
+
+    Attributes
+    ----------
+    evefile : :class:`evefile.boundaries.evefile.EveFile`
+        EveFile the Join should be performed for.
+
+        Although joining is carried out for a small subset of the
+        device data of a evefile, additional information from the
+        evefile may be necessary to perform the task.
+
+    Parameters
+    ----------
+    evefile : :class:`evefile.boundaries.evefile.EveFile`
+        EveFile the join should be performed for.
+
+
+    Examples
+    --------
+    Usually, joining takes place in the :meth:`set_data()
+    <evefile.boundaries.evefile.EveFile.get_joined_data>`
+    method.
+
+    To join data, in case of a detector channel and a motor axis,
+    call :meth:`join` with the respective parameters:
+
+    .. code-block::
+
+        join = Join(evefile=my_evefile)
+        data, *axes = join.join(
+            data=("SimChan:01", None),
+            axes=(("SimMot:02", None)),
+        )
+
+    Note the use of two variables for the return of the method, and in
+    particular the use of ``*axes`` ensuring that ``axes`` is always a list
+    and takes all remaining return arguments, regardless of their count.
+
+    .. important::
+        While it may be tempting to use this class on your own and work
+        further with the returned arrays, you will lose all metadata and
+        context. Hence, simply *don't*. Just use the interface provided by
+        :class:`EveFile <evefile.boundaries.evefile.EveFile>` instead.
+
+    """
+
+    def __init__(self, evefile=None):
+        self.evefile = evefile
+
+    def join(self, data=None, axes=None, scan_module=""):
+        """
+        Harmonise data.
+
+        The main "quantisation" axis of the values for a device and the
+        common reference is the list of positions. Hence, to join,
+        first of all the lists of positions are compared, and gaps handled
+        accordingly.
+
+        As there are different strategies how to deal with gaps in the
+        positions list, generally, there will be different subclasses of the
+        :class:`Join` class dealing each with a particular strategy.
+
+        Parameters
+        ----------
+        data : :class:`tuple` | :class:`list`
+            Name of the device and its attribute data are taken from.
+
+            If the attribute is set to None, ``data`` will be used instead.
+
+        axes : :class:`tuple` | :class:`list`
+            Names of the devices and their attribute axes values are taken from.
+
+            If an attribute is set to None, ``data`` will be used instead.
+
+            Each element of the tuple/list is itself a two-element
+            tuple/list with name and attribute.
+
+        scan_module : :class:`str`
+            Scan module ID the device belongs to
+
+        Returns
+        -------
+        data : :class:`list`
+            Joined data and axes values.
+
+            The first element is always the data, the following the
+            (variable number of) axes. To separate the two and always get a
+            list of axes, you may call it like this:
+
+            .. code-block::
+
+                data, *axes = join.join(...)
+
+        Raises
+        ------
+        ValueError
+            Raised if no evefile is present
+        ValueError
+            Raised if no data are provided
+        ValueError
+            Raised if no axes are provided
+
+        """
+        if not self.evefile:
+            raise ValueError("Need an evefile to join data.")
+        if not data:
+            raise ValueError("Need data to join data.")
+        if not axes:
+            raise ValueError("Need axes to join data.")
+        return self._join(data=data, axes=axes, scan_module=scan_module)
+
+    def _join(self, data=None, axes=None, scan_module=None):  # noqa
+        return []
+
+
+class AxesLastFill(Join):
+    # noinspection PyUnresolvedReferences
+    """
+    Inflate axes to data dimensions using last for missing value.
+
+    This was previously known as "LastFill" mode and was described as "Use
+    all channel data and fill in the last known position for all axes
+    without values." In SQL terms (relational database), this would be
+    similar to a left join with data left and axes right, but additionally
+    explicitly setting the missing axes values in the join to the last
+    known axis value.
+
+    While the terms "channel" and "axis" have different meanings than in
+    context of the :mod:`joining
+    <evefile.controllers.joining>` module, the behaviour is
+    qualitatively similar:
+
+    * The device used as "data" is taken as reference and its values are
+      *not* changed.
+    * The values of  devices used as "axes" are inflated to the same
+      dimension as the data.
+    * For values originally missing for an axis, the last value of the
+      previous position is used.
+    * If no previous value exists for a missing value, the data are
+      converted into a :obj:`numpy.ma.MaskedArray` object and the values
+      masked with :data:`numpy.ma.masked`.
+    * The snapshots are checked for values corresponding to the axis,
+      and if present, are taken into account.
+
+    Of course, as in all cases, the (integer) positions are used as common
+    reference for the values of all devices.
+
+    .. important::
+        If there is more than one snapshot, always the newest snapshot
+        previous to the current axis position should be used. Check whether
+        this is implemented already.
+
+    Attributes
+    ----------
+    evefile : :class:`evefile.boundaries.evefile.EveFile`
+        EveFile the join should be performed for.
+
+        Although joining is carried out for a small subset of the
+        device data of a evefile, additional information from the
+        evefile may be necessary to perform the task, *e.g.*,
+        the snapshots.
+
+    Parameters
+    ----------
+    evefile : :class:`evefile.boundaries.evefile.EveFile`
+        EveFile the join should be performed for.
+
+
+    Examples
+    --------
+    See the :class:`Join` base class for examples -- and replace
+    the class name accordingly.
+
+    """
+
+    def __init__(self, evefile=None):
+        super().__init__(evefile=evefile)
+
+    # pylint: disable-next=too-many-locals
+    def _join(self, data=None, axes=None, scan_module=None):
+        result = []
+        data_device = self.evefile.data[data[0]]
+        axes_devices = [self.evefile.data[axis[0]] for axis in axes]
+        if data[1]:
+            data_attribute = data[1]
+        else:
+            data_attribute = "data"
+        data_values = getattr(data_device, data_attribute)
+        result.append(data_values)
+        for idx, axes_device in enumerate(axes_devices):
+            if axes[idx][1]:
+                axes_attribute = axes[idx][1]
+            else:
+                axes_attribute = "data"
+            values = getattr(axes_device, axes_attribute)
+            if axes[idx][0] in self.evefile.snapshots:
+                self.evefile.snapshots[axes[idx][0]].get_data()
+                axes_positions = np.searchsorted(
+                    axes_device.position_counts,
+                    self.evefile.snapshots[axes[idx][0]].position_counts,
+                )
+                snapshot_values = getattr(
+                    self.evefile.snapshots[axes[idx][0]],
+                    axes_attribute,
+                )
+                values = np.insert(values, axes_positions, snapshot_values)
+            else:
+                axes_positions = axes_device.position_counts
+            positions = (
+                np.digitize(data_device.position_counts, axes_positions) - 1
+            )
+            values = values[positions]
+            # Set values to special value where no previous axis values exist
+            if np.any(np.where(positions < 0)):
+                values = ma.masked_array(values)
+                values[np.where(positions < 0)] = ma.masked
+            result.append(values)
+        return result
+
+
+class JoinFactory:
+    """
+    Factory for getting the correct join object.
+
+    For background on the need for joining, see the documentation of the
+    entire :mod:`joining <evefile.controllers.joining>` module, and
+    of the :class:`Join <evefile.controllers.joining.Join>` class.
+
+    Given a decision which type of join you would like to apply to
+    your data, this factory class allows you to get the correct
+    join instance without hassle. And you can even change your mind
+    in between and don't have to change any code---the whole idea behind
+    factories.
+
+    Attributes
+    ----------
+    evefile : :class:`evefile.boundaries.evefile.EveFile`
+        EveFile the join should be performed for.
+
+
+    Parameters
+    ----------
+    evefile : :class:`evefile.boundaries.evefile.EveFile`
+        EveFile the join should be performed for.
+
+
+    Examples
+    --------
+    Getting a join object is as simple as calling a single method
+    on the factory object:
+
+    .. code-block::
+
+        factory = JoinFactory()
+        join = factory.get_join(mode="AxesLastFill")
+
+    This will provide you with the appropriate :obj:`AxesLastFill` instance.
+
+    As joins need a :class:`EveFile
+    <evefile.boundaries.evefile.EveFile>` object,
+    you can set one to the factory, and it will get added automatically to
+    the join instance for you:
+
+    .. code-block::
+
+        factory = JoinFactory(evefile=my_evefile)
+        join = factory.get_join(mode="AxesLastFill")
+
+    Thus, when used from within a :class:`EveFile
+    <evefile.boundaries.evefile.EveFile>` object,
+    set the :attr:`evefile` attribute to ``self``.
+
+    """
+
+    def __init__(self, evefile=None):
+        self.evefile = evefile
+
+    def get_join(self, mode="Join"):
+        """
+        Obtain a :class:`Join` instance for a particular mode.
+
+        If no mode is provided, this defaults to the base class. As the
+        :class:`Join` does not implement any functionality, this is
+        rather useless.
+
+        If the :attr:`evefile` attribute is set, it is automatically set
+        in the :obj:`Join` instance returned.
+
+        Parameters
+        ----------
+        mode : :class:`str`
+            Join mode to return a :class:`Join` instance for.
+
+            Default: "Join"
+
+        Returns
+        -------
+        join : :class:`Join`
+            Join instance
+
+        """
+        instance = globals()[mode](evefile=self.evefile)
+        return instance
--- tests/controllers/test_joining.py
+++ tests/controllers/test_joining.py
@@ -1,3 +1,271 @@
 import unittest
 
+import numpy as np
+from numpy import ma
+
 from evefile.controllers import joining
+
+
+class MockDevice:
+    def __init__(self, data=np.random.random(5), positions=np.arange(2, 7)):
+        self.data = data
+        self.position_counts = positions
+
+    def get_data(self):
+        pass
+
+
+class MockScanModule:
+    def __init__(self, data=None):
+        self.data = data
+
+
+class MockEveFile:
+    def __init__(self, snapshots=False):
+        self.data = {
+            "SimChan:01": MockDevice(),
+            "SimMot:01": MockDevice(),
+        }
+        if snapshots:
+            self.snapshots = {
+                "SimChan:01": MockDevice(
+                    data=np.random.random(2), positions=np.asarray([1, 7])
+                ),
+                "SimMot:01": MockDevice(
+                    data=np.random.random(2), positions=np.asarray([1, 7])
+                ),
+            }
+        else:
+            self.snapshots = {}
+
+
+class TestJoin(unittest.TestCase):
+    def setUp(self):
+        self.join = joining.Join()
+
+    def test_instantiate_class(self):
+        pass
+
+    def test_has_attributes(self):
+        attributes = [
+            "evefile",
+        ]
+        for attribute in attributes:
+            with self.subTest(attribute=attribute):
+                self.assertTrue(hasattr(self.join, attribute))
+
+    def test_initialise_with_evefile_sets_evefile(self):
+        evefile = "foo"
+        join = joining.Join(evefile=evefile)
+        self.assertEqual(evefile, join.evefile)
+
+    def test_join_without_evefile_raises(self):
+        self.join.evefile = None
+        with self.assertRaisesRegex(
+            ValueError, "Need an evefile to join data."
+        ):
+            self.join.join()
+
+    def test_join_without_data_raises(self):
+        self.join.evefile = "foo"
+        with self.assertRaisesRegex(ValueError, "Need data to join data."):
+            self.join.join()
+
+    def test_join_without_axes_raises(self):
+        self.join.evefile = "foo"
+        with self.assertRaisesRegex(ValueError, "Need axes to join data."):
+            self.join.join(data="foo")
+
+    def test_join_returns_list(self):
+        self.join.evefile = "foo"
+        result = self.join.join(data=("foo", None), axes=("bar", None))
+        self.assertIsInstance(result, list)
+
+
+class TestAxesLastFill(unittest.TestCase):
+    def setUp(self):
+        self.join = joining.AxesLastFill()
+        self.join.evefile = MockEveFile()
+
+    def test_instantiate_class(self):
+        pass
+
+    def test_join_returns_list_of_numpy_arrays(self):
+        result = self.join.join(
+            data=("SimChan:01", None),
+            axes=(("SimMot:01", None),),
+            scan_module="main",
+        )
+        self.assertTrue(result)
+        for element in result:
+            self.assertIsInstance(element, np.ndarray)
+
+    def test_join_returns_only_values_for_data_positions(self):
+        self.join.evefile.data = {
+            "SimChan:01": MockDevice(
+                data=np.random.random(5), positions=np.linspace(0, 4, 5)
+            ),
+            "SimMot:01": MockDevice(
+                data=np.random.random(7), positions=np.linspace(0, 6, 7)
+            ),
+        }
+        result = self.join.join(
+            data=("SimChan:01", None),
+            axes=(("SimMot:01", None),),
+            scan_module="main",
+        )
+        self.assertEqual(len(result[0]), len(result[1]))
+
+    def test_join_returns_only_values_for_data_positions_with_gaps(self):
+        self.join.evefile.data = {
+            "SimChan:01": MockDevice(
+                data=np.random.random(4), positions=np.asarray([0, 2, 3, 4])
+            ),
+            "SimMot:01": MockDevice(
+                data=np.random.random(7), positions=np.linspace(0, 6, 7)
+            ),
+        }
+        result = self.join.join(
+            data=("SimChan:01", None),
+            axes=(("SimMot:01", None),),
+            scan_module="main",
+        )
+        self.assertEqual(len(result[0]), len(result[1]))
+
+    def test_join_fills_axes_values(self):
+        self.join.evefile.data = {
+            "SimChan:01": MockDevice(
+                data=np.random.random(5), positions=np.linspace(0, 4, 5)
+            ),
+            "SimMot:01": MockDevice(
+                data=np.random.random(4), positions=np.linspace(0, 3, 4)
+            ),
+        }
+        result = self.join.join(
+            data=("SimChan:01", None),
+            axes=(("SimMot:01", None),),
+            scan_module="main",
+        )
+        self.assertEqual(len(result[0]), len(result[1]))
+        np.testing.assert_array_equal(
+            self.join.evefile.data["SimMot:01"].data,
+            result[1][:-1],
+        )
+        self.assertEqual(result[1][-2], result[1][-1])
+
+    def test_join_masks_axes_values_with_gap_at_beginning(self):
+        self.join.evefile.data = {
+            "SimChan:01": MockDevice(
+                data=np.random.random(5), positions=np.linspace(0, 4, 5)
+            ),
+            "SimMot:01": MockDevice(
+                data=np.random.random(3), positions=np.linspace(2, 4, 3)
+            ),
+        }
+        result = self.join.join(
+            data=("SimChan:01", None),
+            axes=(("SimMot:01", None),),
+            scan_module="main",
+        )
+        self.assertEqual(len(result[0]), len(result[1]))
+        np.testing.assert_array_equal(
+            self.join.evefile.data["SimMot:01"].data,
+            result[1][2:],
+        )
+        self.assertIsInstance(result[1], ma.masked_array)
+        self.assertTrue(result[1].mask[0])
+
+    def test_join_fills_axes_values_with_gaps(self):
+        self.join.evefile.data = {
+            "SimChan:01": MockDevice(
+                data=np.random.random(5), positions=np.linspace(0, 4, 5)
+            ),
+            "SimMot:01": MockDevice(
+                data=np.random.random(4), positions=np.asarray([0, 2, 4])
+            ),
+        }
+        result = self.join.join(
+            data=("SimChan:01", None),
+            axes=(("SimMot:01", None),),
+            scan_module="main",
+        )
+        self.assertEqual(len(result[0]), len(result[1]))
+        self.assertEqual(result[1][0], result[1][1])
+        self.assertEqual(result[1][2], result[1][3])
+
+    def test_join_with_data_attribute_returns_correct_values(self):
+        result = self.join.join(
+            data=("SimChan:01", "position_counts"),
+            axes=(("SimMot:01", None),),
+            scan_module="main",
+        )
+        np.testing.assert_array_equal(
+            self.join.evefile.data["SimChan:01"].position_counts,
+            result[0],
+        )
+
+    def test_join_with_axes_attribute_returns_correct_values(self):
+        result = self.join.join(
+            data=("SimChan:01", None),
+            axes=(("SimMot:01", "position_counts"),),
+            scan_module="main",
+        )
+        np.testing.assert_array_equal(
+            self.join.evefile.data["SimMot:01"].position_counts,
+            result[1],
+        )
+
+    def test_join_fills_axes_values_with_snapshots(self):
+        self.join.evefile.data = {
+            "SimChan:01": MockDevice(
+                data=np.random.random(5), positions=np.arange(2, 7)
+            ),
+            "SimMot:01": MockDevice(
+                data=np.random.random(4), positions=np.asarray([3, 4, 5, 6])
+            ),
+        }
+        self.join.evefile.snapshots = {
+            "SimChan:01": MockDevice(
+                data=np.random.random(2), positions=np.asarray([1, 7])
+            ),
+            "SimMot:01": MockDevice(
+                data=np.random.random(2), positions=np.asarray([1, 7])
+            ),
+        }
+        result = self.join.join(
+            data=("SimChan:01", None),
+            axes=(("SimMot:01", None),),
+            scan_module="main",
+        )
+        self.assertEqual(len(result[0]), len(result[1]))
+        self.assertEqual(
+            self.join.evefile.snapshots["SimMot:01"].data[0],
+            result[1][0],
+        )
+
+
+class TestJoinFactory(unittest.TestCase):
+    def setUp(self):
+        self.factory = joining.JoinFactory()
+
+    def test_instantiate_class(self):
+        pass
+
+    def test_get_join_returns_join(self):
+        self.assertIsInstance(self.factory.get_join(), joining.Join)
+
+    def test_get_join_with_type_returns_correct_join(self):
+        self.assertIsInstance(
+            self.factory.get_join(mode="AxesLastFill"),
+            joining.Join,
+        )
+
+    def test_initialise_with_evefile_sets_evefile(self):
+        evefile = "foo"
+        factory = joining.JoinFactory(evefile=evefile)
+        self.assertEqual(evefile, factory.evefile)
+
+    def test_get_join_with_evefile_sets_evefile(self):
+        self.factory.evefile = "foo"
+        join = self.factory.get_join()
+        self.assertEqual(self.factory.evefile, join.evefile)
